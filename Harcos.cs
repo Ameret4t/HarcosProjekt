a71bff5c0a5c271bc90ff7903576c156cb0e1738
Ameret4t <marczinko010509@gmail.com>
2020-09-25 10:05:36 +0200

    harcos osztaly + deklaralas

---
+++ Harcos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarcosProjekt
+{
+    class Harcos
+    {
+        private string nev;
+        private int szint;
+        private int tapasztalat;
+        private int alapEletero;
+        private int alapSebzes;
+    }
+}
