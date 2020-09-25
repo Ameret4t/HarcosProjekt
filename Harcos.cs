fa80da2f471a914fd9f25713c7dc6721f07d145f
Ameret4t <marczinko010509@gmail.com>
2020-09-25 10:06:41 +0200

    konstruktor + getter setter

--- Harcos.cs
+++ Harcos.cs
@@ -14,5 +14,20 @@ namespace HarcosProjekt
         private int tapasztalat;
         private int alapEletero;
         private int alapSebzes;
+
+        public Harcos(string nev, int szint, int tapasztalat, int alapEletero, int alapSebzes)
+        {
+            this.nev = nev;
+            this.szint = szint;
+            this.tapasztalat = tapasztalat;
+            this.alapEletero = alapEletero;
+            this.alapSebzes = alapSebzes;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Szint { get => szint; set => szint = value; }
+        public int Tapasztalat { get => tapasztalat; set => tapasztalat = value; }
+        public int AlapEletero { get => alapEletero; set => alapEletero = value; }
+        public int AlapSebzes { get => alapSebzes; set => alapSebzes = value; }
     }
 }
