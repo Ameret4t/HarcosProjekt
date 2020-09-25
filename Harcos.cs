44998197e3bcc0d383d58e26fd367c8d8415e164
Ameret4t <marczinko010509@gmail.com>
2020-09-25 10:36:04 +0200

    konstruktor, getter setterek javitasa

--- Harcos.cs
+++ Harcos.cs
@@ -12,22 +12,43 @@ namespace HarcosProjekt
         private string nev;
         private int szint;
         private int tapasztalat;
+        private int eletero;
         private int alapEletero;
         private int alapSebzes;
 
-        public Harcos(string nev, int szint, int tapasztalat, int alapEletero, int alapSebzes)
+        public Harcos(string nev, int statuszSablon)
         {
             this.nev = nev;
-            this.szint = szint;
-            this.tapasztalat = tapasztalat;
-            this.alapEletero = alapEletero;
-            this.alapSebzes = alapSebzes;
+            this.szint = 1;
+            this.tapasztalat = 0;
+            this.eletero = maxEletero;
+            if (statuszSablon == 1)
+            {
+                this.alapEletero = 15;
+                this.alapSebzes = 3;
+            }
+            else if (statuszSablon == 2)
+            {
+                this.alapEletero = 12;
+                this.alapSebzes = 4;
+            }
+            else
+            {
+                this.alapEletero = 8;
+                this.alapSebzes = 5;
+            }
+            
         }
 
         public string Nev { get => nev; set => nev = value; }
         public int Szint { get => szint; set => szint = value; }
         public int Tapasztalat { get => tapasztalat; set => tapasztalat = value; }
-        public int AlapEletero { get => alapEletero; set => alapEletero = value; }
-        public int AlapSebzes { get => alapSebzes; set => alapSebzes = value; }
+        public int Eletero { get => eletero; set => eletero = value; }
+        public int AlapEletero { get => alapEletero; }
+        public int Sebzes { get => alapSebzes + szint; }
+        public int AlapSebzes { get => alapSebzes; }
+        
+        public int maxEletero { get => alapEletero; }
+        public int szintLepeshez { get =>  10 + szint * 5; }
     }
 }
